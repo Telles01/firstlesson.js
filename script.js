731dc105b8874b546f16bde6923e74c9951ff733
Bernard Telles <bernardtelgon@gmail.com>
2020-11-08 12:04:28 -0300

    learning js

---
+++ script.js
@@ -0,0 +1,30 @@
+var nome = 'bernard';
+
+var idade = 30;
+
+var comidaFavorita;
+
+comidaFavorita = 'pizza';
+
+
+
+
+var time = 'galo doido',
+
+  cidade = 'bh',
+
+  pais = 'brasil';
+
+  bairro = 'floresta';
+
+  continente = 'america';
+
+  console.log(nome,idade,comidaFavorita, time, cidade, pais, bairro);
+
+
+  var bedroom = 'quarto',
+      bed = 'cama',
+      pillow = 'travesseiro',
+      desk = 'mesa';
+      armChair = 'poltrona';
+
